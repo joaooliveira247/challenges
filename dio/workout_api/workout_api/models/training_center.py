167ba2153256ff04061826b4878056c6de37002d
joaooliveira247 <joaooliveira247@gmail.com>
2024-05-05 07:56:42 -0300

    :adhesive_bandage: change training center name to unique

--- dio/workout_api/workout_api/models/training_center.py
+++ dio/workout_api/workout_api/models/training_center.py
@@ -8,7 +8,7 @@ class TrainingCenterModel(BaseModel):
     __tablename__ = "training_center"
 
     pk_id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    name: Mapped[str] = mapped_column(String(50), nullable=True)
+    name: Mapped[str] = mapped_column(String(50), nullable=True, unique=True)
     address: Mapped[str] = mapped_column(String(60), nullable=True)
     owner: Mapped[str] = mapped_column(String(30), nullable=True)
     athlete: Mapped["AthleteModel"] = relationship(
