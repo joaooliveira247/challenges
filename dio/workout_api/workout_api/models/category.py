926ef0f989a3f9532556461f782fcc0e898a13ec
joaooliveira247 <joaooliveira247@gmail.com>
2024-05-04 21:28:24 -0300

    :sparkles: add category model

---
+++ dio/workout_api/workout_api/models/category.py
@@ -0,0 +1,12 @@
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy.types import Integer, String
+
+from workout_api.contrib.models import BaseModel
+
+
+class CategoryModel(BaseModel):
+    __tablename__ = "Category"
+
+    pk_id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String(50), nullable=True)
+    athlete: Mapped["AthleteModel"] = relationship(back_populates="category")
