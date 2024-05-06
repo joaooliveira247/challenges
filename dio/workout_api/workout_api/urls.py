763017a3b740a5ca7df8dad8580cfdb4e3344e1a
joaooliveira247 <joaooliveira247@gmail.com>
2024-05-06 10:28:20 -0300

    :sparkles: add center training router

--- dio/workout_api/workout_api/urls.py
+++ dio/workout_api/workout_api/urls.py
@@ -1,15 +1,24 @@
 from fastapi import APIRouter
 
-from workout_api.controllers import athlete_controller, category_controller
+from workout_api.controllers import (
+    athlete_controller,
+    category_controller,
+    training_center_controller,
+)
 
 api_router = APIRouter()
 api_router.include_router(
     athlete_controller,
     prefix="/athlete",
-    tags=["athletes/"],
+    tags=["Athletes"],
 )
 api_router.include_router(
     category_controller,
     prefix="/category",
-    tags=["category/"],
+    tags=["Categories"],
+)
+api_router.include_router(
+    training_center_controller,
+    prefix="/training_center",
+    tags=["Training Centers"],
 )
