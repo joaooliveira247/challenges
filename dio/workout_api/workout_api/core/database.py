869fb810f8d703654a6015965140188098dd5621
joaooliveira247 <joaooliveira247@gmail.com>
2024-05-06 09:56:42 -0300

    :bug: fix reference session to callable

--- dio/workout_api/workout_api/core/database.py
+++ dio/workout_api/workout_api/core/database.py
@@ -5,10 +5,9 @@ from sqlalchemy.orm import sessionmaker
 
 engine = create_async_engine(settings.DB_URL, echo=False)
 
-async_session = sessionmaker(
-    engine, class_=AsyncSession, expire_on_commit=False
-)
+async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
 
 async def get_session() -> AsyncGenerator:
-    async with async_session as session:
+    async with async_session() as session:
         yield session
