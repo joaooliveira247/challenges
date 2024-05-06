de459d1e91871faf23841f9be3e6ce962db58568
joaooliveira247 <joaooliveira247@gmail.com>
2024-05-05 21:44:17 -0300

    :sparkles: add imports in __all__

--- dio/workout_api/workout_api/core/__init__.py
+++ dio/workout_api/workout_api/core/__init__.py
@@ -1,3 +1,5 @@
 from workout_api.core.config import settings, BASE_DIR
+from workout_api.core.database import get_session
+from workout_api.core.dependencies import DatabaseDependency
 
-__all__ = ["settings", "BASE_DIR"]
+__all__ = ["settings", "BASE_DIR", "get_session", "DatabaseDependency"]
