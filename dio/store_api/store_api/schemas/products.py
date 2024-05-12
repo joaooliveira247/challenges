1afdf4c95da6bc162cb937ef82520a29095a1107
joaooliveira247 <joaooliveira247@gmail.com>
2024-05-11 21:25:32 -0300

    :sparkles: add ProductIn schema

---
+++ dio/store_api/store_api/schemas/products.py
@@ -0,0 +1,10 @@
+from pydantic import Field, PositiveFloat, PositiveInt
+
+from store_api.schemas.base import BaseSchemaMixin
+
+
+class ProductIn(BaseSchemaMixin):
+    name: str = Field(..., description="Product name")
+    quantity: PositiveInt = Field(..., description="Product quantity")
+    price: PositiveFloat = Field(..., description="Product price")
+    status: bool = Field(..., description="Product status")
