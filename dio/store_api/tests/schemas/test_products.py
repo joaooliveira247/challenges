9982d6e862e79a6b79379776cc188bbf624a0e14
joaooliveira247 <joaooliveira247@gmail.com>
2024-05-11 21:35:41 -0300

    :test_tube: add fail test with ValidationError from pydantic

--- dio/store_api/tests/schemas/test_products.py
+++ dio/store_api/tests/schemas/test_products.py
@@ -1,5 +1,7 @@
 from uuid import UUID
 
+from pydantic import ValidationError
+from pytest import raises
 
 from store_api.schemas import ProductIn
 from tests.factories import product_data
@@ -11,3 +13,18 @@ def test_schemas_return_success():
 
     assert product.name == "Iphone 14 Pro Max"
     assert isinstance(product.id, UUID)
+
+
+def test_schemas_raise_validation_error():
+    data = product_data()
+    data.pop("status")
+    with raises(ValidationError) as err:
+        ProductIn.model_validate(data)
+
+    assert err.value.errors()[0] == {
+        "type": "missing",
+        "loc": ("status",),
+        "msg": "Field required",
+        "input": {"name": "Iphone 14 Pro Max", "quantity": 10, "price": 8500.0},
+        "url": "https://errors.pydantic.dev/2.7/v/missing",
+    }
