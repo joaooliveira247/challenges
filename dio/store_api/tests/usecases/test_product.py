c0d86ef44134dd45005ea63eda4a4961c07576d8
joaooliveira247 <joaooliveira247@gmail.com>
2024-05-13 14:42:26 -0300

    :white_check_mark: add get return success

--- dio/store_api/tests/usecases/test_product.py
+++ dio/store_api/tests/usecases/test_product.py
@@ -1,9 +1,18 @@
 from store_api.usecases import product_usecase
+from store_api.schemas import ProductOut
 
 
-async def test_usecases_should_return_sucess(product_in):
+async def test_usecases_create_should_return_sucess(product_in):
     result = await product_usecase.create(body=product_in)
 
-    # assert isinstance(result, ProductOut)
+    assert isinstance(result, ProductOut)
+    assert result.name == "Iphone 14 Pro Max"
 
-    assert result is None
+
+async def test_usecases_get_should_return_sucess(product_in, product_id):
+    # here i create a product 'cause fixture clear_collections delete all data
+    await product_usecase.create(body=product_in)
+    result = await product_usecase.get(id=product_id)
+
+    assert isinstance(result, ProductOut)
+    assert result.name == "Iphone 14 Pro Max"
