7e6a28d510b9814098774848b5582724c590eb46
joaooliveira247 <joaooliveira247@gmail.com>
2024-05-13 15:59:57 -0300

    :white_check_mark: add update return success test

--- dio/store_api/tests/usecases/test_product.py
+++ dio/store_api/tests/usecases/test_product.py
@@ -3,7 +3,7 @@ from uuid import UUID
 from pytest import raises
 
 from store_api.core.exceptions import DBNotFoundValueException
-from store_api.schemas import ProductOut
+from store_api.schemas import ProductOut, ProductUpdateOut
 from store_api.usecases import product_usecase
 
 
@@ -33,3 +33,19 @@ async def test_usecases_get_should_return_not_found():
         err.value.message
         == "Product not found with UUID(9b6efc62-acda-44fb-9f5f-9f86aa3efbd3)"
     )
+
+
+async def test_usecases_query_should_return_success():
+    result = await product_usecase.query()
+
+    assert isinstance(result, list)
+
+
+async def test_usecases_update_should_return_success(
+    product_id, product_in, product_up
+):
+    await product_usecase.create(body=product_in)
+    product_up.price = 7500.00
+    result = await product_usecase.update(id=product_id, body=product_up)
+
+    assert isinstance(result, ProductUpdateOut)
