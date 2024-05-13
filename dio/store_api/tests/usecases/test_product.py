0ff581e613fcf1fc5dc4beea76ee984a364f3cf4
joaooliveira247 <joaooliveira247@gmail.com>
2024-05-13 18:18:43 -0300

    :boom: change manual create do fixture

--- dio/store_api/tests/usecases/test_product.py
+++ dio/store_api/tests/usecases/test_product.py
@@ -14,9 +14,7 @@ async def test_usecases_create_should_return_sucess(product_in):
     assert result.name == "Iphone 14 Pro Max"
 
 
-async def test_usecases_get_should_return_success(product_in, product_id):
-    # here i create a product 'cause fixture clear_collections delete all data
-    await product_usecase.create(body=product_in)
+async def test_usecases_get_should_return_success(product_inserted, product_id):
     result = await product_usecase.get(id=product_id)
 
     assert isinstance(result, ProductOut)
@@ -42,24 +40,21 @@ async def test_usecases_query_should_return_success():
 
 
 async def test_usecases_update_should_return_success(
-    product_id, product_in, product_up
+    product_id, product_inserted, product_up
 ):
-    await product_usecase.create(body=product_in)
     product_up.price = 7500.00
     result = await product_usecase.update(id=product_id, body=product_up)
 
     assert isinstance(result, ProductUpdateOut)
 
 
-async def test_usecases_delete_return_success(product_id, product_in):
-    await product_usecase.create(product_in)
-
+async def test_usecases_delete_return_success(product_id, product_inserted):
     result = await product_usecase.delete(id=product_id)
 
     assert result is True
 
 
-async def test_usecases_delete_should_return_not_found(product_id):
+async def test_usecases_delete_should_return_not_found():
     with raises(DBNotFoundValueException) as err:
         await product_usecase.get(
             id=UUID("9b6efc62-acda-44fb-9f5f-9f86aa3efbd3"),
