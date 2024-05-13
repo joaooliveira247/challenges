3fe292e872f5f61df2961a954a790da553dd37e4
joaooliveira247 <joaooliveira247@gmail.com>
2024-05-13 18:34:39 -0300

    :sparkles: add factorie return list of products

--- dio/store_api/tests/factories.py
+++ dio/store_api/tests/factories.py
@@ -5,3 +5,32 @@ def product_data() -> dict:
         "price": 8500.00,
         "status": True,
     }
+
+
+def products_data() -> list[dict]:
+    return [
+        {
+            "name": "Iphone 15 Pro Max",
+            "quantity": 3,
+            "price": 10500.00,
+            "status": False,
+        },
+        {
+            "name": "Iphone 13 Pro Max",
+            "quantity": 5,
+            "price": 6500.00,
+            "status": True,
+        },
+        {
+            "name": "Iphone 12 Pro Max",
+            "quantity": 15,
+            "price": 5500.00,
+            "status": True,
+        },
+        {
+            "name": "Iphone 11 Pro Max",
+            "quantity": 20,
+            "price": 4500.00,
+            "status": True,
+        },
+    ]
