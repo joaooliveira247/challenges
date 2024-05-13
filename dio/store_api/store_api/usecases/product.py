cbb24a386d5f2ba320eed568f88fdf9a346404ef
joaooliveira247 <joaooliveira247@gmail.com>
2024-05-13 15:01:51 -0300

    :sparkles: add get method

--- dio/store_api/store_api/usecases/product.py
+++ dio/store_api/store_api/usecases/product.py
@@ -1,6 +1,10 @@
+from uuid import UUID
+
 from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorDatabase
+
+from store_api.core.exceptions import DBNotFoundValueException
 from store_api.db import db_client
-from store_api.schemas import ProductIn
+from store_api.schemas import ProductIn, ProductOut
 
 
 class ProductUseCase:
@@ -9,8 +13,19 @@ class ProductUseCase:
         self.database: AsyncIOMotorDatabase = self.cliente.get_database()
         self.collection = self.database.get_collection("products")
 
-    async def create(self, body: ProductIn) -> None:
-        await self.collection.insert_one(body.model_dump())
+    async def create(self, body: ProductIn) -> ProductOut:
+        product = ProductOut(**body.model_dump())
+        await self.collection.insert_one(product.model_dump())
+        return product
+
+    async def get(self, id: UUID) -> ProductOut:
+        result = await self.collection.find_one({"id": id})
+
+        if not result:
+            raise DBNotFoundValueException(
+                f"Product not found with UUID({id})",
+            )
+        return ProductOut(**result)
 
 
 product_usecase = ProductUseCase()
