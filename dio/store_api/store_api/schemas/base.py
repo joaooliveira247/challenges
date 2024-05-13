82be5bc07a7fcf46f7414e2c9683ea74184c312c
joaooliveira247 <joaooliveira247@gmail.com>
2024-05-13 18:43:26 -0300

    :truck: move code to models

--- dio/store_api/store_api/schemas/base.py
+++ dio/store_api/store_api/schemas/base.py
@@ -1,9 +1,6 @@
-from pydantic import BaseModel, UUID4, Field
-from uuid import uuid4
-from datetime import datetime
+from pydantic import BaseModel
 
 
 class BaseSchemaMixin(BaseModel):
-    id: UUID4 = Field(default_factory=uuid4)
-    created_at: datetime = Field(default_factory=datetime.now)
-    updated_at: datetime = Field(default_factory=datetime.now)
+    class Config:
+        from_attributes = True
