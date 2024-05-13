bc39fe43523970047c1dd6e0d5dc380bbf19d794
joaooliveira247 <joaooliveira247@gmail.com>
2024-05-13 15:36:52 -0300

    :truck: change ProductIn to ProductBase

--- dio/store_api/store_api/schemas/products.py
+++ dio/store_api/store_api/schemas/products.py
@@ -3,12 +3,12 @@ from pydantic import Field, PositiveFloat, PositiveInt
 from store_api.schemas.base import BaseSchemaMixin
 
 
-class ProductIn(BaseSchemaMixin):
+class ProductBase(BaseSchemaMixin):
     name: str = Field(..., description="Product name")
     quantity: PositiveInt = Field(..., description="Product quantity")
     price: PositiveFloat = Field(..., description="Product price")
     status: bool = Field(..., description="Product status")
 
 
-class ProductOut(ProductIn):
+class ProductOut(ProductBase):
     ...
