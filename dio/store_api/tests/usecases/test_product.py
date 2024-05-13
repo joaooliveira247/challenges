d78d56ea48793ff520f08505d6c5474149a0d7ae
joaooliveira247 <joaooliveira247@gmail.com>
2024-05-13 15:00:21 -0300

    :test_tube: add get return not found

--- dio/store_api/tests/usecases/test_product.py
+++ dio/store_api/tests/usecases/test_product.py
@@ -1,5 +1,10 @@
-from store_api.usecases import product_usecase
+from uuid import UUID
+
+from pytest import raises
+
+from store_api.core.exceptions import DBNotFoundValueException
 from store_api.schemas import ProductOut
+from store_api.usecases import product_usecase
 
 
 async def test_usecases_create_should_return_sucess(product_in):
@@ -9,10 +14,22 @@ async def test_usecases_create_should_return_sucess(product_in):
     assert result.name == "Iphone 14 Pro Max"
 
 
-async def test_usecases_get_should_return_sucess(product_in, product_id):
+async def test_usecases_get_should_return_success(product_in, product_id):
     # here i create a product 'cause fixture clear_collections delete all data
     await product_usecase.create(body=product_in)
     result = await product_usecase.get(id=product_id)
 
     assert isinstance(result, ProductOut)
     assert result.name == "Iphone 14 Pro Max"
+
+
+async def test_usecases_get_should_return_not_found():
+    with raises(DBNotFoundValueException) as err:
+        await product_usecase.get(
+            id=UUID("9b6efc62-acda-44fb-9f5f-9f86aa3efbd3"),
+        )
+
+    assert (
+        err.value.message
+        == "Product not found with UUID(9b6efc62-acda-44fb-9f5f-9f86aa3efbd3)"
+    )
