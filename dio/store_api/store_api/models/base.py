52eb8994aafb77d39c796b3c10d451e1dd369001
joaooliveira247 <joaooliveira247@gmail.com>
2024-05-14 14:36:44 -0300

    :sparkles: add model serializer

--- dio/store_api/store_api/models/base.py
+++ dio/store_api/store_api/models/base.py
@@ -1,10 +1,23 @@
 from datetime import datetime
 from uuid import uuid4
+from typing import Any
+from decimal import Decimal
+from bson import Decimal128
 
-from pydantic import UUID4, BaseModel, Field
+from pydantic import UUID4, BaseModel, Field, model_serializer
 
 
 class CreateBaseModel(BaseModel):
     id: UUID4 = Field(default_factory=uuid4)
     created_at: datetime = Field(default_factory=datetime.now)
     updated_at: datetime = Field(default_factory=datetime.now)
+
+    @model_serializer
+    def set_model(self) -> dict[str, Any]:
+        model_dict = dict(self)
+
+        for key, value in model_dict.items():
+            if isinstance(value, Decimal):
+                model_dict[key] = Decimal128(str(value))
+
+        return model_dict
