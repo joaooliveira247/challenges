dd89c038701bcf3a105d1308b35311ada483455f
joaooliveira247 <joaooliveira247@gmail.com>
2024-05-14 11:27:32 -0300

    :sparkles: change ProductSchemaIn to ProductModel in database

--- dio/store_api/store_api/usecases/product.py
+++ dio/store_api/store_api/usecases/product.py
@@ -5,6 +5,7 @@ from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorDatabase
 from store_api.core.exceptions import DBNotFoundValueException
 from store_api.db import db_client
 from store_api.schemas import ProductIn, ProductOut, ProductUpdate, ProductUpdateOut
+from store_api.models import ProductModel
 
 from pymongo import ReturnDocument
 
@@ -16,9 +17,9 @@ class ProductUseCase:
         self.collection = self.database.get_collection("products")
 
     async def create(self, body: ProductIn) -> ProductOut:
-        product = ProductOut(**body.model_dump())
+        product = ProductModel(**body.model_dump())
         await self.collection.insert_one(product.model_dump())
-        return product
+        return ProductOut(**product.model_dump())
 
     async def get(self, id: UUID) -> ProductOut:
         result = await self.collection.find_one({"id": id})
