ad4524454094d3102416d54cc0b7ae68a190e14e
joaooliveira247 <joaooliveira247@gmail.com>
2024-05-14 14:37:07 -0300

    :sparkles: add OutMixin

--- dio/store_api/store_api/schemas/base.py
+++ dio/store_api/store_api/schemas/base.py
@@ -1,5 +1,22 @@
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel, ConfigDict, UUID4, Field, model_validator
+from datetime import datetime
+from bson import Decimal128
+from decimal import Decimal
 
 
 class BaseSchemaMixin(BaseModel):
     model_config = ConfigDict(from_attributes=True)
+
+
+class OutMixin(BaseModel):
+    id: UUID4 = Field()
+    created_at: datetime = Field()
+    updated_at: datetime = Field()
+
+    @model_validator(mode="before")
+    def set_schema(cls, data: dict):
+        for key, value in data.items():
+            if isinstance(value, Decimal128):
+                data[key] = Decimal(str(value))
+
+        return data
