a0dc0caf1e01f861693c65d5c5ba2e722aa6d1bf
joaooliveira247 <joaooliveira247@gmail.com>
2024-05-15 17:35:17 -0300

    :sparkles: add get by id endpoint

--- dio/store_api/store_api/controllers/product.py
+++ dio/store_api/store_api/controllers/product.py
@@ -1,6 +1,8 @@
-from fastapi import APIRouter, status, Body, Depends
+from fastapi import APIRouter, status, Body, Depends, HTTPException, Path
 from store_api.schemas.products import ProductIn, ProductOut
 from store_api.usecases.product import ProductUseCase
+from pydantic import UUID4
+from store_api.core.exceptions import DBNotFoundValueException
 
 product_controller = APIRouter(tags=["products"])
 
@@ -11,3 +13,16 @@ async def post(
     usecase: ProductUseCase = Depends(),
 ) -> ProductOut:
     return await usecase.create(body=body)
+
+
+@product_controller.get(path="/{id}", status_code=status.HTTP_200_OK)
+async def get(
+    id: UUID4 = Path(...),
+    usecase: ProductUseCase = Depends(),
+) -> ProductOut:
+    try:
+        return await usecase.get(id)
+    except DBNotFoundValueException:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail=f"{id} not found."
+        )
