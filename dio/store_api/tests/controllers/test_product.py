17d2a6fd14f7fc1d162ecb3a8fd97e53e3ddf422
joaooliveira247 <joaooliveira247@gmail.com>
2024-05-14 21:09:03 -0300

    :white_check_mark: add test to products post endpoint

---
+++ dio/store_api/tests/controllers/test_product.py
@@ -0,0 +1,8 @@
+from tests.factories import product_data
+from fastapi import status
+
+
+async def test_controller_create_should_return_sucess(client, products_url):
+    response = await client.post(products_url, json=product_data())
+
+    assert response.status_code == status.HTTP_201_CREATED
