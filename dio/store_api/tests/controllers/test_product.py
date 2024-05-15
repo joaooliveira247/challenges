2f07deb172e7aa7129f737ffb12652c1220e1b6c
joaooliveira247 <joaooliveira247@gmail.com>
2024-05-15 17:06:46 -0300

    :white_check_mark: create assert in test_controller_create_should_return_sucess to compare return value

--- dio/store_api/tests/controllers/test_product.py
+++ dio/store_api/tests/controllers/test_product.py
@@ -5,4 +5,10 @@ from fastapi import status
 async def test_controller_create_should_return_sucess(client, products_url):
     response = await client.post(products_url, json=product_data())
 
+    content: dict = response.json()
+    content = {
+        k: v for k, v in content.items() if k not in ["id", "created_at", "updated_at"]
+    }
+
     assert response.status_code == status.HTTP_201_CREATED
+    assert content == product_data()
