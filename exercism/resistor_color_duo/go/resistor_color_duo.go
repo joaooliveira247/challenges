343d6f2dc90931443c498acca2daa6bebcf26b92
joaooliveira247 <joaooliveira247@gmail.com>
2024-09-29 17:59:08 -0300

    :sparkles: add Resistor Color Duo challenge

---
+++ exercism/resistor_color_duo/go/resistor_color_duo.go
@@ -0,0 +1,18 @@
+package resistorcolorduo
+
+// Value should return the resistance value of a resistor with a given colors.
+func Value(colors []string) int {
+	resistors := map[string]int{
+		"black":  0,
+		"brown":  1,
+		"red":    2,
+		"orange": 3,
+		"yellow": 4,
+		"green":  5,
+		"blue":   6,
+		"violet": 7,
+		"grey":   8,
+		"white":  9,
+	}
+	return resistors[colors[0]]*10 + resistors[colors[1]]
+}
