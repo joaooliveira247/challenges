ed329af633c9686d1a76346899d562a32c750291
joaooliveira247 <joaooliveira247@gmail.com>
2025-06-12 14:56:27 -0300

    :sparkles: add discont_calc

---
+++ dio/code_challenges/discont_calc.py
@@ -0,0 +1,13 @@
+descontos = {
+    "DESCONTO10": 0.10,
+    "DESCONTO20": 0.20,
+    "SEM_DESCONTO": 0.00
+}
+
+# Entrada do usuário
+preco = float(input().strip())
+cupom = input().strip()
+
+
+print(f"{(preco - (preco * descontos[cupom])):.2f}")
+
