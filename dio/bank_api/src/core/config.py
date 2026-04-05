ae361dc00b43b002b86e7780d4903c2b8d2e452d
joaooliveira247 <joaooliveira247@gmail.com>
2026-04-05 18:59:47 -0300

    :sparkles: add get_settings function

--- dio/bank_api/src/core/config.py
+++ dio/bank_api/src/core/config.py
@@ -1,3 +1,5 @@
+from functools import lru_cache
+
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
@@ -9,3 +11,8 @@ class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         env_file=".env", env_file_encoding="utf-8"
     )
+
+
+@lru_cache
+def get_settings() -> Settings:
+    return Settings()
