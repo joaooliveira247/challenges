c3a6bb2bc414cdd9e30e5afe938382d1ea27bdff
joaooliveira247 <joaooliveira247@gmail.com>
2026-04-05 18:58:49 -0300

    :sparkles: add Settings class

---
+++ dio/bank_api/src/core/config.py
@@ -0,0 +1,11 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    APP_NAME: str = ""
+    API_HOST: str = "localhost"
+    API_PORT: int = 8000
+
+    model_config = SettingsConfigDict(
+        env_file=".env", env_file_encoding="utf-8"
+    )
