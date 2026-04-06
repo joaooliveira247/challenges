ec17300f27f06d9107f42bb7be57a48f5db1cac7
joaooliveira247 <joaooliveira247@gmail.com>
2026-04-06 10:08:28 -0300

    :sparkles: add postgres_dsn method in Settings class

--- dio/bank_api/src/core/config.py
+++ dio/bank_api/src/core/config.py
@@ -18,6 +18,10 @@ class Settings(BaseSettings):
         env_file=".env", env_file_encoding="utf-8"
     )
 
+    @property
+    def postgres_dsn(self) -> str:
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
 
 @lru_cache
 def get_settings() -> Settings:
