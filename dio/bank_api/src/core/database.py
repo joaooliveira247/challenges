3039bbf1189d55dd76735a2a26e3f3afdd5198aa
joaooliveira247 <joaooliveira247@gmail.com>
2026-04-06 10:34:18 -0300

    :sparkles: add session instance in database

--- dio/bank_api/src/core/database.py
+++ dio/bank_api/src/core/database.py
@@ -1,5 +1,7 @@
 from sqlalchemy.ext.asyncio import (
     AsyncEngine,
+    AsyncSession,
+    async_sessionmaker,
     create_async_engine,
 )
 
@@ -8,3 +10,7 @@ from src.core.config import get_settings
 settings = get_settings()
 
 engine: AsyncEngine = create_async_engine(settings.postgres_dsn)
+
+async_session: async_sessionmaker[AsyncSession] = async_sessionmaker(
+    engine, class_=AsyncSession, expire_on_commit=False
+)
