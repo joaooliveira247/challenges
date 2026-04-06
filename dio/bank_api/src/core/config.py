b4afd03c29b390aea77799ed31d74d9365e4514a
joaooliveira247 <joaooliveira247@gmail.com>
2026-04-06 10:07:51 -0300

    :sparkles: add DB settings vars

--- dio/bank_api/src/core/config.py
+++ dio/bank_api/src/core/config.py
@@ -8,6 +8,12 @@ class Settings(BaseSettings):
     API_HOST: str = "localhost"
     API_PORT: int = 8000
 
+    DB_USER: str | None = None
+    DB_PASSWD: str | None = None
+    DB_HOST: str | None = None
+    DB_PORT: str | None = None
+    DB_NAME: str | None = None
+
     model_config = SettingsConfigDict(
         env_file=".env", env_file_encoding="utf-8"
     )
