3088e42d531b9f73aa10e2fc8512721a99001c5c
joaooliveira247 <joaooliveira247@gmail.com>
2026-04-06 10:33:32 -0300

    :sparkles: add engine instance in database

--- dio/bank_api/src/core/database.py
+++ dio/bank_api/src/core/database.py
@@ -1,3 +1,10 @@
+from sqlalchemy.ext.asyncio import (
+    AsyncEngine,
+    create_async_engine,
+)
+
 from src.core.config import get_settings
 
 settings = get_settings()
+
+engine: AsyncEngine = create_async_engine(settings.postgres_dsn)
