0a29b4700dbb1460297dd96778c781745f5f8b8e
joaooliveira247 <joaooliveira247@gmail.com>
2026-04-06 10:35:39 -0300

    :sparkles: add get_session function in database

--- dio/bank_api/src/core/database.py
+++ dio/bank_api/src/core/database.py
@@ -4,6 +4,7 @@ from sqlalchemy.ext.asyncio import (
     async_sessionmaker,
     create_async_engine,
 )
+from typing_extensions import AsyncGenerator
 
 from src.core.config import get_settings
 
@@ -14,3 +15,8 @@ engine: AsyncEngine = create_async_engine(settings.postgres_dsn)
 async_session: async_sessionmaker[AsyncSession] = async_sessionmaker(
     engine, class_=AsyncSession, expire_on_commit=False
 )
+
+
+async def get_session() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session() as session:
+        yield session
