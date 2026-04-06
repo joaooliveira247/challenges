dc2f00a5ca8cbb9f83d931c8839328fff7325bfc
joaooliveira247 <joaooliveira247@gmail.com>
2026-04-06 16:53:10 -0300

    :sparkles: add contrib BaseModel class

---
+++ dio/bank_api/bank_api/contrib/models.py
@@ -0,0 +1,27 @@
+from uuid import uuid4
+
+from sqlalchemy import func
+from sqlalchemy.dialects.postgresql import UUID as PG_UUID
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.sql.sqltypes import TIMESTAMP, UUID
+
+
+class BaseModel(DeclarativeBase):
+    id: Mapped[UUID] = mapped_column(
+        PG_UUID(as_uuid=True),
+        default=uuid4,
+        nullable=False,
+        primary_key=True,
+        unique=True,
+    )
+    created_at: Mapped[TIMESTAMP] = mapped_column(
+        TIMESTAMP,
+        default=func.now(),
+        nullable=False,
+    )
+    updated_at: Mapped[TIMESTAMP] = mapped_column(
+        TIMESTAMP,
+        default=func.now(),
+        onupdate=func.now(),
+        nullable=False,
+    )
