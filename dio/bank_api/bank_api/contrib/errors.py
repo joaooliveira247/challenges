f5491e5912f84f18adb262045f9b63cf499865d1
joaooliveira247 <joaooliveira247@gmail.com>
2026-04-09 18:43:04 -0300

    :sparkles: add DatabaseError in contrib.errors

--- dio/bank_api/bank_api/contrib/errors.py
+++ dio/bank_api/bank_api/contrib/errors.py
@@ -2,4 +2,8 @@ class BaseError(Exception):
     def __init__(self, message: str) -> None:
         super().__init__(message)
         self.message = message
-        
+
+
+class DatabaseError(BaseError):
+    def __init__(self, message: str, resource: str) -> None:
+        super().__init__(f"[Database] ({resource}): {message}")
