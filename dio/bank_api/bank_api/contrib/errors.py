553fb2046415cef4247467953ad89098b639b9cc
joaooliveira247 <joaooliveira247@gmail.com>
2026-04-10 16:31:27 -0300

    :sparkles: add UnexpectedError in contrib.errors

--- dio/bank_api/bank_api/contrib/errors.py
+++ dio/bank_api/bank_api/contrib/errors.py
@@ -4,6 +4,11 @@ class BaseError(Exception):
         self.message = message
 
 
+class UnexpectedError(BaseError):
+    def __init__(self, message: str, location: str, resource: str) -> None:
+        super().__init__(f"[{location}] ({resource}: {message})")
+
+
 class DatabaseError(BaseError):
     def __init__(self, message: str, resource: str) -> None:
         super().__init__(f"[Database] ({resource}): {message}")
