cda125ae170a7ba4f6a860a5806167864267b2bc
joaooliveira247 <joaooliveira247@gmail.com>
2026-04-12 15:48:38 -0300

    :bug: change type annotation Mapped[Enum] to Mapped[AccountStatusEnum] and Mapped[AccountRoleEnum]

--- dio/bank_api/bank_api/models/accounts.py
+++ dio/bank_api/bank_api/models/accounts.py
@@ -27,13 +27,13 @@ class AccountModel(BaseModel):
     )
     ssn: Mapped[CHAR] = mapped_column(CHAR(11), unique=True, nullable=False)
     balance: Mapped[BIGINT] = mapped_column(BIGINT, default=0)
-    status: Mapped[Enum] = mapped_column(
+    status: Mapped[AccountStatusEnum] = mapped_column(
         Enum(AccountStatusEnum, name="status_enum", native_enum=True),
         default=AccountStatusEnum.ACTIVATED,
         nullable=False,
     )
     password: Mapped[str] = mapped_column(VARCHAR(60), nullable=False)
-    role: Mapped[Enum] = mapped_column(
+    role: Mapped[AccountRoleEnum] = mapped_column(
         Enum(AccountRoleEnum, name="role_enum", native_enum=True),
         default=AccountRoleEnum.CUSTOMER,
         nullable=False,
