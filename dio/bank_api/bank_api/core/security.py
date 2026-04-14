94aa67f101fe8c7ae3c0215e4f95c37df331ce66
joaooliveira247 <joaooliveira247@gmail.com>
2026-04-14 18:20:03 -0300

    :sparkles: add fastapi oauth2_schema in core.security

--- dio/bank_api/bank_api/core/security.py
+++ dio/bank_api/bank_api/core/security.py
@@ -1,3 +1,4 @@
+from fastapi.security import OAuth2PasswordBearer
 from passlib.context import CryptContext
 
 from bank_api.core.config import get_settings
@@ -5,3 +6,7 @@ from bank_api.core.config import get_settings
 settings = get_settings()
 
 pwd_context: CryptContext = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+oauth2_schema: OAuth2PasswordBearer = OAuth2PasswordBearer(
+    tokenUrl=f"{settings.API_PATH}/account/sign-in"
+)
