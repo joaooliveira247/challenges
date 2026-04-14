281ed55706d3a4e43657bbc88e64d315c06922eb
joaooliveira247 <joaooliveira247@gmail.com>
2026-04-14 18:10:23 -0300

    :sparkles: add pwd_context CryptContext instance in core.security

--- dio/bank_api/bank_api/core/security.py
+++ dio/bank_api/bank_api/core/security.py
@@ -1,4 +1,7 @@
+from passlib.context import CryptContext
+
 from bank_api.core.config import get_settings
 
 settings = get_settings()
 
+pwd_context: CryptContext = CryptContext(schemes=["bcrypt"], deprecated="auto")
