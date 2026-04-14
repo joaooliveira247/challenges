27d726e768fcbdb778159e18c2e6244647285c9a
joaooliveira247 <joaooliveira247@gmail.com>
2026-04-14 18:24:09 -0300

    :sparkles: add gen_hash func in core.security

--- dio/bank_api/bank_api/core/security.py
+++ dio/bank_api/bank_api/core/security.py
@@ -10,3 +10,10 @@ pwd_context: CryptContext = CryptContext(schemes=["bcrypt"], deprecated="auto")
 oauth2_schema: OAuth2PasswordBearer = OAuth2PasswordBearer(
     tokenUrl=f"{settings.API_PATH}/account/sign-in"
 )
+
+
+def gen_hash(passwd: str) -> str | None:
+    if not isinstance(passwd, str):
+        raise ValueError("password must be a string")
+        return
+    return pwd_context.hash(passwd)
