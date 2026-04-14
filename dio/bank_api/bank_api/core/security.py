473725590d29dc95c683458e547d8150a2446fb0
joaooliveira247 <joaooliveira247@gmail.com>
2026-04-14 19:10:40 -0300

    :sparkles: add check_password in core.security

--- dio/bank_api/bank_api/core/security.py
+++ dio/bank_api/bank_api/core/security.py
@@ -1,6 +1,7 @@
 from fastapi.security import OAuth2PasswordBearer
 from passlib.context import CryptContext
 
+from bank_api.contrib.errors import PasswordHashError
 from bank_api.core.config import get_settings
 
 settings = get_settings()
@@ -17,3 +18,10 @@ def gen_hash(passwd: str) -> str | None:
         raise ValueError("password must be a string")
         return
     return pwd_context.hash(passwd)
+
+
+def check_password(passwd: str, hash_passwd: str) -> bool:
+    try:
+        return pwd_context.verify(passwd, hash_passwd)
+    except Exception as e:
+        raise PasswordHashError(resource="security", message=str(e))
