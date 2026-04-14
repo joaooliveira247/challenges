b2042c10325bce45bb79de1ddab19dfedbd04d7e
joaooliveira247 <joaooliveira247@gmail.com>
2026-04-14 09:17:52 -0300

    :sparkles: add validate_password pydantic cls method in PasswordMixin

--- dio/bank_api/bank_api/schemas/accounts.py
+++ dio/bank_api/bank_api/schemas/accounts.py
@@ -43,3 +43,28 @@ class PasswordMixin(BaseModel):
     password: str = Field(
         ..., description="Account password", min_length=8, max_length=128
     )
+
+    @field_validator("password")
+    def validate_password(cls, value: str) -> str:
+        pattern = {
+            "lower": r"[a-z]",
+            "upper": r"[A-Z]",
+            "digit": r"\d",
+            "special": r"[@$!%*?&]",
+        }
+
+        components = {
+            key: "".join(re.findall(regex, value)) or None
+            for key, regex in pattern.items()
+        }
+
+        missing_components = [
+            key for key, value in components.items() if value is None
+        ]
+
+        if missing_components:
+            raise ValueError(
+                f"Wrong password format! characters missing: {', '.join(missing_components)}"
+            )
+
+        return value
