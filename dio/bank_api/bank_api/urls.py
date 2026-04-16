7de51cbde61ef0a5927ae9372996798017bcc331
joaooliveira247 <joaooliveira247@gmail.com>
2026-04-16 12:56:39 -0300

    :sparkles: add accounts_controller in api_router with prefix /account

--- dio/bank_api/bank_api/urls.py
+++ dio/bank_api/bank_api/urls.py
@@ -1,3 +1,6 @@
 from fastapi import APIRouter
 
+from bank_api.controllers.accounts import accounts_controller
+
 api_router = APIRouter()
+api_router.include_router(accounts_controller, prefix="/account")
