9acf648531cba9f772e2a518773c9f2fefc9c2ee
joaooliveira247 <joaooliveira247@gmail.com>
2026-04-16 13:26:06 -0300

    :sparkles: add instance of Settings in urls

--- dio/bank_api/bank_api/urls.py
+++ dio/bank_api/bank_api/urls.py
@@ -1,6 +1,9 @@
 from fastapi import APIRouter
 
 from bank_api.controllers.accounts import accounts_controller
+from bank_api.core.config import get_settings
+
+settings = get_settings()
 
 api_router = APIRouter()
 api_router.include_router(accounts_controller, prefix="/account")
