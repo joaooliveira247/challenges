705c8c7be555e1d315083e50beb49d6c16b49116
joaooliveira247 <joaooliveira247@gmail.com>
2026-04-16 18:18:57 -0300

    :bug: fix call instance of hash lib

--- dio/bank_api/bank_api/core/security.py
+++ dio/bank_api/bank_api/core/security.py
@@ -1,12 +1,12 @@
 from fastapi.security import OAuth2PasswordBearer
-from passlib.context import CryptContext
+from pwdlib import PasswordHash
 
 from bank_api.contrib.errors import PasswordHashError
 from bank_api.core.config import get_settings
 
 settings = get_settings()
 
-pwd_context: CryptContext = CryptContext(schemes=["bcrypt"], deprecated="auto")
+pwd_context: PasswordHash = PasswordHash.recommended()
 
 oauth2_schema: OAuth2PasswordBearer = OAuth2PasswordBearer(
     tokenUrl=f"{settings.API_PATH}/account/sign-in"
