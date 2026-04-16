92bb78c34eca95397f01d07267c3f1209c869adf
joaooliveira247 <joaooliveira247@gmail.com>
2026-04-16 13:28:42 -0300

    :sparkles: add prefix in api_router

--- dio/bank_api/bank_api/urls.py
+++ dio/bank_api/bank_api/urls.py
@@ -5,5 +5,5 @@ from bank_api.core.config import get_settings
 
 settings = get_settings()
 
-api_router = APIRouter()
+api_router = APIRouter(prefix=settings.API_PATH)
 api_router.include_router(accounts_controller, prefix="/account")
