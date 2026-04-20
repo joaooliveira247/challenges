5f562c9ffd253405d24b8b6546f642fc21467cb0
joaooliveira247 <joaooliveira247@gmail.com>
2026-04-20 18:20:00 -0300

    :recycle: refactor change arg in gen_jwt, now lifetime is getting from .env file

--- dio/bank_api/bank_api/core/token.py
+++ dio/bank_api/bank_api/core/token.py
@@ -11,10 +11,11 @@ from bank_api.models.accounts import AccountModel
 settings = get_settings()
 
 
-def gen_jwt(life_time: float, account: AccountModel) -> str:
+def gen_jwt(account: AccountModel) -> str:
     payload: dict[str, Any] = {
         "sub": str(account.id),
-        "exp": datetime.now(timezone.utc) + timedelta(minutes=life_time),
+        "exp": datetime.now(timezone.utc)
+        + timedelta(minutes=settings.JWT_DEFAULT_LIFE_TIME),
         "iat": datetime.now(timezone.utc),
         "role": account.role,
     }
