857cc5592f742b03a23897f7e9897f336e26c06a
joaooliveira247 <joaooliveira247@gmail.com>
2026-04-20 10:31:56 -0300

    :sparkles: add verify_token_jwt in core.token

--- dio/bank_api/bank_api/core/token.py
+++ dio/bank_api/bank_api/core/token.py
@@ -1,18 +1,37 @@
 from datetime import datetime, timedelta, timezone
 from typing import Any
+
+from jose import jwt
+from jose.exceptions import ExpiredSignatureError, JWTError
+
+from bank_api.contrib.errors import TokenError, UnexpectedError
 from bank_api.core.config import get_settings
 from bank_api.models.accounts import AccountModel
-from jose import jwt
-
 
 settings = get_settings()
+
 
 def gen_jwt(life_time: float, account: AccountModel) -> str:
     payload: dict[str, Any] = {
         "sub": str(account.id),
         "exp": datetime.now(timezone.utc) + timedelta(minutes=life_time),
         "iat": datetime.now(timezone.utc),
-        "role": account.role
+        "role": account.role,
     }
-    
+
     return jwt.encode(payload, settings.JWT_SECRET_KEY, settings.JWT_ALGORITHM)
+
+
+def verify_token_jwt(token: str) -> dict[str, Any] | None:
+    try:
+        decoded = jwt.decode(
+            token, settings.JWT_SECRET_KEY, settings.JWT_ALGORITHM
+        )
+        return decoded
+    except (ExpiredSignatureError, JWTError) as e:
+        raise TokenError(
+            str(e),
+            "core.token",
+        )
+    except Exception as e:
+        raise UnexpectedError(str(e), "TokenError", "core.token")
