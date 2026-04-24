6e7247f2e6a66ccda55af274bba30020cc23d59f
joaooliveira247 <joaooliveira247@gmail.com>
2026-04-24 15:52:30 -0300

    :sparkles: add TransactionStatusEnum enum in models.transactions

--- dio/bank_api/bank_api/models/transaction.py
+++ dio/bank_api/bank_api/models/transaction.py
@@ -1,12 +1,20 @@
+import enum
 from uuid import UUID
 
 from sqlalchemy import ForeignKey
 from sqlalchemy.dialects.postgresql import UUID as PG_UUID
 from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy.sql.sqltypes import Enum
 from sqlalchemy.types import BIGINT
 
 from bank_api.contrib.models import BaseModel
 from bank_api.models.accounts import AccountModel
+
+
+class TransactionStatusEnum(enum.StrEnum):
+    NO_RESOURSES = "no_resourses"
+    REFUND = "refund"
+    ACCEPT = "accept"
 
 
 class TransactionModel(BaseModel):
@@ -14,8 +22,8 @@ class TransactionModel(BaseModel):
 
     amount: Mapped[BIGINT] = mapped_column(BIGINT, nullable=False)
     balance_after: Mapped[BIGINT] = mapped_column(BIGINT, nullable=False)
+
     account_id: Mapped[UUID] = mapped_column(
         PG_UUID(as_uuid=True), ForeignKey("accounts.id"), nullable=False
     )
-
     account: Mapped[AccountModel] = relationship(AccountModel, lazy="selectin")
