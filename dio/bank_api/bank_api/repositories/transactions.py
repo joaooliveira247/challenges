8ec98f0df015c7d74a7d1bf3b037fb75993a981f
joaooliveira247 <joaooliveira247@gmail.com>
2026-04-26 18:26:19 -0300

    :sparkles: add create method in TransactionsRepository in repositories.transactions

--- dio/bank_api/bank_api/repositories/transactions.py
+++ dio/bank_api/bank_api/repositories/transactions.py
@@ -1,8 +1,30 @@
+from uuid import UUID
+
+from sqlalchemy.exc import IntegrityError, OperationalError
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from bank_api.contrib.errors import DatabaseError, UnexpectedError
 from bank_api.contrib.repositories import BaseRepository
+from bank_api.models.transaction import TransactionModel
 
 
 class TransactionsRepository(BaseRepository):
     def __init__(self, db: AsyncSession) -> None:
         super().__init__(db)
+
+    async def create(self, transaction: TransactionModel) -> UUID:
+        try:
+            self.db.add(transaction)
+            await self.db.flush()
+            await self.db.commit()
+            return transaction.id
+        except (OperationalError, IntegrityError) as e:
+            await self.db.rollback()
+            raise DatabaseError(str(e), self.__class__.__name__)
+        except Exception as e:
+            await self.db.rollback()
+            raise UnexpectedError(
+                str(e), location="database", resource=self.__class__.__name__
+            )
+        finally:
+            await self.db.close()
