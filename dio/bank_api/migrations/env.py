328a30967e1b60652ee9395a58afdb3e9e326d04
joaooliveira247 <joaooliveira247@gmail.com>
2026-04-27 09:04:25 -0300

    :bug: add import of alembic_postgresql_enum 'causing bug in enum

--- dio/bank_api/migrations/env.py
+++ dio/bank_api/migrations/env.py
@@ -5,6 +5,7 @@ from alembic import context
 from sqlalchemy import pool
 from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
+import alembic_postgresql_enum
 
 from bank_api.contrib.models import BaseModel
 from bank_api.core.config import get_settings
