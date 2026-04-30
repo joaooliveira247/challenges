f278cfd911541acf841a18de0b857f7b4fe68a89
joaooliveira247 <joaooliveira247@gmail.com>
2026-04-30 17:59:55 -0300

    :sparkles: add deposit controller to /api/v1/transaction/withdrawal

--- dio/bank_api/bank_api/controllers/transactions.py
+++ dio/bank_api/bank_api/controllers/transactions.py
@@ -54,3 +54,63 @@ async def deposit(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail=str(e),
         )
+
+
+transactions_controller.post("/withdrawal")
+
+
+async def withdrawal(
+    db: DatabaseDependency,
+    account: CurrentAccount,
+    amount: Money,
+) -> Money:
+    transactions_repository = TransactionsRepository(db)
+    accounts_repository = AccountsRepository(db)
+
+    amount_cents = int(amount * 100)
+
+    if account.balance < amount_cents:
+        transaction = TransactionModel(
+            account_id=account.id,
+            amount=amount_cents,
+            balance_after=account.balance,
+            status=TransactionStatusEnum.REFUND,
+            type=TransactionTypeEnum.WITHDRAWAL,
+        )
+
+        await transactions_repository.create(transaction)
+
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Insufficient balance",
+        )
+
+    new_balance = account.balance - amount_cents
+
+    transaction = TransactionModel(
+        account_id=account.id,
+        amount=amount_cents,
+        balance_after=new_balance,
+        status=TransactionStatusEnum.ACCEPT,
+        type=TransactionTypeEnum.WITHDRAWAL,
+    )
+
+    try:
+        await accounts_repository.update_balance(
+            account_id=account.id,
+            amount=new_balance,
+        )
+
+        await transactions_repository.create(transaction)
+
+        return new_balance
+
+    except (DatabaseError, UnexpectedError) as e:
+        transaction.status = TransactionStatusEnum.REFUND
+
+        await transactions_repository.create(transaction)
+
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=str(e),
+        )
