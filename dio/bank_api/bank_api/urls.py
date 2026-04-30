b5dd17bfbc482f9eb12a141289139135fd955741
joaooliveira247 <joaooliveira247@gmail.com>
2026-04-30 18:01:24 -0300

    :sparkles: add route /transaction in urls.py

--- dio/bank_api/bank_api/urls.py
+++ dio/bank_api/bank_api/urls.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter
 
 from bank_api.controllers.accounts import accounts_controller
+from bank_api.controllers.transactions import transactions_controller
 from bank_api.core.config import get_settings
 
 settings = get_settings()
 
 api_router = APIRouter(prefix=settings.API_PATH)
 api_router.include_router(accounts_controller, prefix="/account")
+api_router.include_router(transactions_controller, prefix="/transaction")
