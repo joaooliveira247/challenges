dd2e8adcc6a859bac18b003e1d4f7d865ae2b4ea
joaooliveira247 <joaooliveira247@gmail.com>
2026-05-28 13:44:41 -0300

    :sparkles: add your_age challenge

---
+++ dio/code_challenges/your_age/src/main.rs
@@ -0,0 +1,7 @@
+const CURRENT_YEAR: u16 = 2026;
+
+fn main() {
+    /* Given a year calcule the age of people */
+    let birth_year: u16 = 1998;
+    println!("Your age is: {} years old", CURRENT_YEAR - birth_year)
+}
