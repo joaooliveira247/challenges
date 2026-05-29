3be73b3d66537d0b3c3f62334b3025510e497b66
joaooliveira247 <joaooliveira247@gmail.com>
2026-05-29 14:15:51 -0300

    :sparkles: add control flux logic

--- dio/code_challenges/your_age/src/main.rs
+++ dio/code_challenges/your_age/src/main.rs
@@ -1,7 +1,19 @@
-const CURRENT_YEAR: u16 = 2026;
+const CURRENT_DATE: (u8, u8, u16) = (29, 5, 2026);
 
 fn main() {
     /* Given a year calcule the age of people */
+    let name: &str = "John";
     let birth_year: u16 = 1998;
-    println!("Your age is: {} years old", CURRENT_YEAR - birth_year)
+    let birth_month: u8 = 4;
+    let birth_day: u8 = 16;
+
+    let mut age: u16 = CURRENT_DATE.2 - birth_year;
+
+    if CURRENT_DATE.1 < birth_month {
+        age -= 1;
+    } else if CURRENT_DATE.0 < birth_day && CURRENT_DATE.1 == birth_month {
+        age -= 1;
+    }
+
+    println!("{} age is: {} years old", name, age)
 }
