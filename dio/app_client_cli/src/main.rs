ef1178e0bc997e7743f4af72d124482d32dcb25b
joaooliveira247 <joaooliveira247@gmail.com>
2026-06-11 09:41:17 -0300

    :sparkles: change main call

--- dio/app_client_cli/src/main.rs
+++ dio/app_client_cli/src/main.rs
@@ -1,12 +1,5 @@
-use app_client_cli::Customer;
+use app_client_cli::show_menu;
 
 fn main() {
-    let customer_1 = Customer::new(
-        1,
-        String::from("John"),
-        String::from("123.456.789-0"),
-        String::from("Rua dos bobos, n° 8"),
-    );
-
-    println!("{:?}", customer_1);
+    show_menu();
 }
